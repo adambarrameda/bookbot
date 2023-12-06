c1846f0fe434aae7fcf0b500392baf85f9277216
adambarrameda <hello@adambarrameda.com>
2023-12-05 22:57:31 -0500

    Report function added

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    # print(file_contents)
-    words = file_contents.split()
-    num_words = len(words)
-    print(num_words)
+def main():
+    path = "books/frankenstein.txt"
+    text = get_text(path)
+    report(text, path)
+    
+def get_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def count_letters(text):
+    text = text.lower()
+    letter_count = {}
+    for char in text:
+        if char in letter_count:
+            letter_count[char] += 1
+        else:
+            letter_count[char] = 1
+    return letter_count
+
+def report(text, filepath):
+    word_count = count_words(text)
+    letter_count = count_letters(text)
+    sorted_letters = sorted(letter_count.items(), key=lambda x: x[1], reverse=True)
+    print(f"--- Begin report of {filepath} ---")
+    print(f"Document contains {word_count} words.")
+    for char in sorted_letters:
+        if char[0].isalpha():
+            print(f"The '{char[0]}' character appears {char[1]} times.")
+    print("--- End Report ---")
+
+main()
